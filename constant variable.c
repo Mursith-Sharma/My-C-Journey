a071d79bbddfcb982a35f82b89fa5d309feafc3d
CYBER BOY ツ <mmursith12345@gmail.com>
2025-03-15 15:31:07 +0530

    Constants Variable

---
+++ constant variable.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#define PI 3.14
+
+int main()
+{
+
+    float area;
+    int radius = 5; // OR you can use this --->  const int radius = 5;
+    area = PI * radius * radius;
+    printf("Area = %f\n",area);
+
+    /* Constants Variable types
+       1. #define
+       2. const
+    */
+}
