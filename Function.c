d45ddfc8b7aff6a695f335171654c6cd629d2e05
CYBER BOY ツ <mmursith12345@gmail.com>
2025-03-16 22:46:42 +0530

    Function.c

--- Function.c
+++ Function.c
@@ -8,3 +8,37 @@ int main() {
   messi();  // call the function
   return 0;
 }
+
+
+//Real Scenario
+
+#include<stdio.h>
+
+int messi()
+{
+    int a=40,b;
+    b=a+30;
+    printf("a = %d\n",b);
+}
+
+int ronaldo()
+{
+    int c=40,d;
+    d=c+40;
+    printf("a = %d\n",d);
+}
+
+int neymar()
+{
+    int x=40,y;
+    y=x+50;
+    printf("a = %d\n",y);
+}
+
+int main()
+{
+    messi();
+    ronaldo();
+    neymar();
+}
+
