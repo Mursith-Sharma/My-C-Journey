e395abd722a5f3f0053abcf0d8e967470ff7a4b3
CYBER BOY ツ <mmursith12345@gmail.com>
2025-03-16 13:43:30 +0530

    Primary Data Type.c

--- Primary Data Type.c
+++ Primary Data Type.c
@@ -1,8 +1,18 @@
 
-Data Type            	Size	               Description	Example
+Data Type            	Size	               Description                                                                                                                  	Example
 
 
-int	               2 or 4 bytes            Stores whole numbers, without decimals	1
-float	             4 bytes	               Stores fractional numbers, containing one or more decimals. Sufficient for storing 6-7 decimal digits	1.99
-double	           8 bytes	               Stores fractional numbers, containing one or more decimals. Sufficient for storing 15 decimal digits	1.99
-char	             1 byte	                 Stores a single character/letter/number, or ASCII values	'A'
+int	               2 or 4 bytes            Stores whole numbers, without decimals                                                                                          	1
+float	             4 bytes	               Stores fractional numbers, containing one or more decimals. Sufficient for storing 6-7 decimal digits                           	1.99
+double	           8 bytes	               Stores fractional numbers, containing one or more decimals. Sufficient for storing 15 decimal digits                           	1.99
+char	             1 byte	                 Stores a single character/letter/number, or ASCII values	                                                                        'A'
+
+  #include<stdio.h>
+  int main ()
+{
+  printf("size of int = %d\n" , sizeof(int));
+  printf("size of char = %d\n" , sizeof(char));
+  printf("size of float = %d\n" , sizeof(float));
+  printf("size of double = %d\n" , sizeof(double));
+  
+}
