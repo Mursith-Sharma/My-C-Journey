7c91054ac7aca4098fddecec1082fef91bfb0093
CYBER BOY ツ <mmursith12345@gmail.com>
2025-06-02 13:14:47 +0530

    Delete welcome.php

--- welcome.php
+++
@@ -1,9 +0,0 @@
-<html>
-<body>
-
-Welcome <?php echo $_POST["name"]; ?><br>                            <!-- Welcome mursith-->
-Your email address is: <?php echo $_GET["email"]; ?>                 <!-- Your email address is: rruttuyar@gmail.com-->
-     
-
-</body>
-</html>
