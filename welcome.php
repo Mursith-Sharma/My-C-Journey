68803f7c322e595c0128d7b3a0960cf2ed150db3
CYBER BOY ツ <mmursith12345@gmail.com>
2025-06-02 13:14:32 +0530

    Add files via upload

---
+++ welcome.php
@@ -0,0 +1,9 @@
+<html>
+<body>
+
+Welcome <?php echo $_POST["name"]; ?><br>                            <!-- Welcome mursith-->
+Your email address is: <?php echo $_GET["email"]; ?>                 <!-- Your email address is: rruttuyar@gmail.com-->
+     
+
+</body>
+</html>
